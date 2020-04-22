35e97a4671e599337044f0266803367504f7d7f4
tannercyost <42818307+tannercyost@users.noreply.github.com>
2020-04-22 11:55:12 -0700

    Implemented spawn points for items

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public List<GameObject> rows;
+    private List<Transform> childs;
+    public GameObject temp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        childs = new List<Transform>();
+        GetAllChildren(transform, ref childs);
+        //foreach (GameObject item in rows)
+        //{
+        //    Debug.Log(item);
+        //}
+
+        foreach (Transform item in childs)
+        {
+            Instantiate(temp, item);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private static void GetAllChildren(Transform parent, ref List<Transform> transforms)
+    {
+
+        foreach (Transform t in parent)
+        {
+
+            transforms.Add(t);
+
+            GetAllChildren(t, ref transforms);
+
+        }
+
+    }
+}
