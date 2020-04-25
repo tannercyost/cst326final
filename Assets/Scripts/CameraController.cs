25e204f28668765b3756e2b0840843598b0b3ff5
tannercyost <42818307+tannercyost@users.noreply.github.com>
2020-04-25 16:12:09 -0700

    Implemented camera follow and player movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    private Transform sphere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sphere = player.transform.GetChild(0);
+        offset = transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        
+        transform.position = new Vector3(sphere.position.x + offset.x, transform.position.y, transform.position.z);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    public float speed; 
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveVertical, 0, -moveHorizontal);
+
+        rb.AddForce(movement * speed);
+    }
 }
